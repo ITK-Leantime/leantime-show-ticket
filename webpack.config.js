0fa7d258595569fd8baf7a7d833c8a3a85594640
Sine Jespersen <sinejespersen@gmail.com>
2025-04-16 14:49:24 +0200

    add tinymce to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: './assets/show-ticket.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'show-ticket.js',
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-          patterns: [
-            {
-              from: path.resolve(__dirname, 'assets/show-ticket.css'),
-              to: path.resolve(__dirname, 'dist'),
-            },
-          ],
-        }),
+  entry: "./assets/show-ticket.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "show-ticket.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "assets/show-ticket.css"),
+          to: path.resolve(__dirname, "dist"),
+        },
       ],
-    mode: 'development',
+    }),
+  ],
+  resolve: {
+    alias: {
+      tinymce: path.resolve(__dirname, "node_modules", "tinymce"),
+    },
+  },
+  mode: "development",
 };
