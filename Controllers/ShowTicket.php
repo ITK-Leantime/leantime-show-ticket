2279776a0f34be2f9a6a351af1a7045455a3ac68
Sine Jespersen <sinejespersen@gmail.com>
2025-05-20 14:29:28 +0200

    delete comment, add comment, reply to comment and add comments/worklogs to ui

--- Controllers/ShowTicket.php
+++ Controllers/ShowTicket.php
@@ -7,8 +7,10 @@ use Illuminate\Contracts\Container\BindingResolutionException;
 use Leantime\Core\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Leantime\Plugins\ShowTicket\Services\ShowTicket as ShowTicketService;
+use Leantime\Plugins\ShowTicket\Repositories\ShowTicket as ShowTicketRepository;
 use Leantime\Core\UI\Template;
 use Leantime\Domain\Users\Services\Users as UserService;
+use Leantime\Domain\Comments\Repositories\Comments as CommentRepository;
 use Illuminate\Http\JsonResponse as JsonResponse;
 use Leantime\Domain\Sprints\Services\Sprints as SprintService;
 use Leantime\Domain\Tickets\Services\Tickets as TicketService;
@@ -27,6 +29,8 @@ class ShowTicket extends Controller
     private SprintService $sprintService;
     private TicketService $ticketService;
     private FileRepository $filesRepo;
+    private CommentRepository $commentRepository;
+    private ShowTicketRepository $showTicketRepository;
 
     /**
      * constructor
@@ -39,7 +43,7 @@ class ShowTicket extends Controller
      * @param FileRepository    $filesRepo
      * @return void
      */
-    public function init(ShowTicketService $showTicketService, TicketService $ticketService, SprintService $sprintService, UserService $userService, Template $template, FileRepository $filesRepo): void
+    public function init(ShowTicketService $showTicketService, TicketService $ticketService, SprintService $sprintService, UserService $userService, Template $template, FileRepository $filesRepo, CommentRepository $commentRepository, ShowTicketRepository $showTicketRepository): void
     {
         $this->showTicketService = $showTicketService;
         $this->ticketService = $ticketService;
@@ -47,6 +51,8 @@ class ShowTicket extends Controller
         $this->userService = $userService;
         $this->template = $template;
         $this->filesRepo = $filesRepo;
+        $this->commentRepository = $commentRepository;
+        $this->showTicketRepository = $showTicketRepository;
     }
 
     /**
@@ -66,10 +72,9 @@ class ShowTicket extends Controller
     }
 
     /**
-     * Saves ticket headline.
+     * Deletes ticket.
      * @param string[] $input The input for saving:
      *                     - 'id': The id.
-     *                     - 'headline': The headline.
      * @return JsonResponse The JSON response containing the list of tickets or an empty array.
      */
     public function deleteTicket(array $input): JsonResponse
@@ -79,6 +84,51 @@ class ShowTicket extends Controller
         }
         $deleteResult = $this->showTicketService->deleteTicket($input['id']);
         return response()->json(['ticket' => $deleteResult]);
+    }
+
+    /**
+     * Deletes comment.
+     * @param string[] $input The input for saving:
+     *                     - 'id': The id.
+     * @return JsonResponse The JSON response containing a boolean.
+     */
+    public function deleteComment(array $input): JsonResponse
+    {
+        if (!AuthService::userIsAtLeast(Roles::$editor)) {
+            return  response()->json(['error' => true]);
+        }
+        $deleteResult = $this->showTicketService->deleteComment($input['id']);
+        return response()->json(['ticket' => $deleteResult]);
+    }
+    /**
+     * Edit comment.
+     * @param string[] $input The input for saving:
+     *                     - 'id': The id.
+     * @return JsonResponse The JSON response containing a boolean.
+     */
+    public function editComment(array $input): JsonResponse
+    {
+        if (!AuthService::userIsAtLeast(Roles::$editor)) {
+            return  response()->json(['error' => true]);
+        }
+
+        $editResult = $this->showTicketService->editComment($input);
+        return response()->json(['ticket' => $editResult]);
+    }
+    /**
+     * Reply to comment.
+     * @param string[] $input The input for saving:
+     *                     - 'id': The id.
+     * @return JsonResponse The JSON response containing a boolean.
+     */
+    public function replyToComment(array $input): JsonResponse
+    {
+        if (!AuthService::userIsAtLeast(Roles::$editor)) {
+            return  response()->json(['error' => true]);
+        }
+
+        $editResult = $this->showTicketService->replyToComment($input);
+        return response()->json(['ticket' => $editResult]);
     }
 
     /**
@@ -158,6 +208,39 @@ class ShowTicket extends Controller
 
                 $subtasks = $this->showTicketService->getAllSubtasks($ticket->id);
                 $this->template->assign('subtasks', $subtasks);
+
+                $comments = $this->commentRepository->getComments('ticket', $ticket->id);
+
+                // In leantime, a comment has 'replies', which means in the database, there a comments
+                // that have a "commentParent" set to the comments id, and these are found through the
+                // commentrepo->getReplies
+                foreach ($comments as &$comment) {
+                    $comment['replies'] = [];
+                    $replies = $this->commentRepository->getReplies($comment['id']);
+                    foreach ($replies as &$reply) {
+                        // Display date, this can probably be done in a better way...
+                        $reply['display_date'] = dtHelper()->parseDbDateTime($reply['date'])->setToUserTimezone()->format('Y-m-d H:i:s');
+                        $reply['editable'] = session('userdata.id') === $reply['userId'];
+                    }
+                    $comment['replies'] = $replies;
+                    $comment['editable'] = session('userdata.id') === $comment['userId'];
+                    $comment['display_date'] = dtHelper()->parseDbDateTime($comment['date'])->setToUserTimezone()->format('Y-m-d H:i:s');
+                }
+
+                $worklogs = $this->showTicketRepository->getTimesheetsByTicket($ticket->id);
+
+                foreach ($worklogs as &$worklog) {
+                    // Display date, this can probably be done in a better way...
+                    $worklog['display_date'] = dtHelper()->parseDbDateTime($worklog['workdate'])->setToUserTimezone()->format('Y-m-d');
+
+                    // Username for display
+                    $userId = $worklog['userid'];
+                    $userThatLogged = array_column($users, null, 'id')[$userId] ?? false;
+                    $worklog['display_name'] = $userThatLogged ? $userThatLogged['firstname'] . ' ' . $userThatLogged['lastname'] : 'Ukendt bruger';
+                }
+
+                $this->template->assign('comments', $comments);
+                $this->template->assign('worklogs', $worklogs);
 
                 $this->template->assign('ticketExists', true);
                 $this->template->assign('ticket', $ticket);
